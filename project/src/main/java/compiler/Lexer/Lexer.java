5ddc63e3ac5560e434b1f468d49d6ece7f1d4971
euanlc <euan.l.cockburn@gmail.com>
2026-02-26 16:01:28 +0100

    laptop -> pc

--- project/src/main/java/compiler/Lexer/Lexer.java
+++ project/src/main/java/compiler/Lexer/Lexer.java
@@ -1,12 +1,46 @@
 package compiler.Lexer;
 import java.io.Reader;
+import Symbol from compiler.Lexer.Symbol;
+
+//TODO: define language
+//      check if char is in language, if not throw an error
+//      identify symbol type
 
 public class Lexer {
-    
+    private Reader input;
     public Lexer(Reader input) {
+        this.input = input;
     }
-    
+    private String SimpleMatch(){
+        int state = 0; // Initial state
+        List<Character> buffer = new ArrayList<>(); // Buffer to hold characters for the current symbol
+        while(true) {
+            char nextChar = (char) input.read();
+            if (nextChar == -1) {
+                break; // End of file
+            }
+            if (state == 0) {
+                // Write out NFA logic here, add to buffer if transition is valid, throw error if not
+            }
+        }
+        return new String(buffer.toArray(new Character[0])); // Convert buffer to string and return
+    }
+
     public Symbol getNextSymbol() {
-        return null;
+        // if nextchar is whitespace, skip it
+        // if nextchar is # skip all characters until next eol
+        char ch = (char) input.read();
+        if (Character.isWhitespace(ch)) {
+            // Skip whitespace
+            return getNextSymbol();
+        }
+        if (ch == '#') {
+            // Skip comment until end of line
+            while ((ch = (char) input.read()) != '\n' && ch != -1) {
+                // Continue skipping until newline or end of file
+            }
+            return getNextSymbol(); // Return next symbol after comment
+        }
+        return new Symbol("TEST");
     }
 }
--- project/src/main/java/compiler/Lexer/Symbol.java
+++ project/src/main/java/compiler/Lexer/Symbol.java
@@ -1,4 +1,28 @@
 package compiler.Lexer;
 
+// TODO: define symbol types
+//       decide how to represent value (just string maybe?)
+
 public class Symbol {
+    private String type;
+    private String value;
+
+    private String IdentifyType(String value) {
+        if (value.equals("NULL")) {
+            return "NULL";
+        }
+        // Add more type identification logic here
+        return "UNKNOWN";
+    }
+
+    public Symbol(String value) {
+        this.type = IdentifyType(value);
+        this.value = value;
+    }
+
+    // Overiding the toString method to return the type and value of the symbol as <TYPE, value>
+    @Override
+    public String toString() {
+        return "<" + type + ", " + value + ">";
+    }
 }
